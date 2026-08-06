787cefd53588b23017cfa879fe611748877c6eb4
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-08-06 22:26:19 +0500

    aws complete configuration + env

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,11 @@
 using backend.Data;
 using backend.Services;
 using Microsoft.EntityFrameworkCore;
+using Amazon.S3;
+using Amazon;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +19,26 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<ICodeGenerator, CodeGenerator>();
 
+builder.Services.AddSingleton<IAmazonS3>(sp =>
+{
+    var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY")
+        ?? throw new Exception("AWS_ACCESS_KEY is missing.");
+
+    var secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY")
+        ?? throw new Exception("AWS_SECRET_KEY is missing.");
+
+    var region = Environment.GetEnvironmentVariable("AWS_REGION")
+        ?? throw new Exception("AWS_REGION is missing.");
+
+    return new AmazonS3Client(
+        accessKey,
+        secretKey,
+        RegionEndpoint.GetBySystemName(region)
+    );
+});
+
+builder.Services.AddScoped<IS3Service, S3Service>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,29 +50,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
