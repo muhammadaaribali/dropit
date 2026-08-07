6d2a0e9334466b18aae7e1b0303404d66250b1ad
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-08-07 16:36:22 +0500

    file upload to aws

--- backend/Services/S3Service.cs
+++ backend/Services/S3Service.cs
@@ -1,21 +1,61 @@
 using Amazon.S3;
 using Amazon.S3.Transfer;
+using Amazon.S3.Model;
 
 namespace backend.Services;
 
 public class S3Service : IS3Service
 {
     private readonly IAmazonS3 _s3Client;
-    private readonly IConfiguration _configuration;
+    private readonly string _bucketName;
 
-    public S3Service(IAmazonS3 s3Client, IConfiguration configuration)
+    public S3Service(IAmazonS3 s3Client)
     {
         _s3Client = s3Client;
-        _configuration = configuration;
+        _bucketName = Environment.GetEnvironmentVariable("AWS_BUCKET_NAME") ?? throw new Exception("AWS_BUCKET_NAME missing");
     }
 
     public async Task<string> UploadFileAsync(IFormFile file)
     {
-        throw new NotImplementedException();
+        var fileExtension= Path.GetExtension(file.FileName);
+        var objectKey = $"uploads/{Guid.NewGuid()}{fileExtension}";
+        using var stream = file.OpenReadStream();
+
+        var request = new PutObjectRequest
+        {
+            BucketName = _bucketName,
+            Key = objectKey,
+            InputStream = stream,
+            ContentType= file.ContentType
+        };
+
+        await _s3Client.PutObjectAsync(request);
+
+        return objectKey;
     }
 }
+
+/*
+User selects cat.png
+        │
+        ▼
+Browser uploads file
+        │
+        ▼
+ASP.NET Core creates IFormFile
+        │
+        ▼
+OpenReadStream()
+        │
+        ▼
+A stream is opened for reading the file
+        │
+        ▼
+PutObjectAsync()
+        │
+        ▼
+AWS repeatedly reads bytes from the stream
+        │
+        ▼
+File stored in S3
+*/
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba82cb1190ad8a5c9a4643176331bc198be6db0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787cefd53588b23017cfa879fe611748877c6eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
